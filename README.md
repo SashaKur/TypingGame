c7853de707997ad99e998a4d5b708d40b1d8ebb5
SashaKur <101345117+SashaKur@users.noreply.github.com>
2023-08-02 15:28:01 +0200

    Project initialization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TypingGame
+
 Game implemented in JavaFX that measures your typing speed in interactive manner
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.olek.typinggame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.olek.typinggame to javafx.fxml;
+    exports com.olek.typinggame;
+}
